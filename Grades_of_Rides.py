df5c5687e77b758288abe8275cd04da5f0afb5c5
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-04-26 14:41:17 +0530

    hf,sf,sp=map(int,input().split()) if hf>50 and sf>60 and sp>100:     print(10) elif hf>50 and sf>60:     print(9) elif sf>60 and sp>100:     print(8) elif hf>50 and sp>100:     print(7) elif hf>50 or sf>60 or sp>100:     print(6) else:     print(5)

---
+++ Grades_of_Rides.py
@@ -0,0 +1,13 @@
+hf,sf,sp=map(int,input().split())
+if hf>50 and sf>60 and sp>100:
+    print(10)
+elif hf>50 and sf>60:
+    print(9)
+elif sf>60 and sp>100:
+    print(8)
+elif hf>50 and sp>100:
+    print(7)
+elif hf>50 or sf>60 or sp>100:
+    print(6)
+else:
+    print(5)
