1298cf23d33207b9fc088327dc5f989e20c823e9
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-05-14 21:36:43 +0530

    a=int(input()) even=odd=0 while a:     r=a%10     a=a//10     if r%2==0:         even+=1     else:         odd+=1 if even>0 and odd==0:     print("Even") elif even==0 and odd>0:     print("Odd") else:     print("Mixed")

---
+++ Even_Odd_Mixed.py
@@ -0,0 +1,15 @@
+a=int(input())
+even=odd=0
+while a:
+    r=a%10
+    a=a//10
+    if r%2==0:
+        even+=1
+    else:
+        odd+=1
+if even>0 and odd==0:
+    print("Even")
+elif even==0 and odd>0:
+    print("Odd")
+else:
+    print("Mixed")
