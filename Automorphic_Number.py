026a1a41ac8ad54833d2ddbca42e28646a07915c
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-06-28 07:30:52 +0530

    n=int(input()) x=n c=0 while x:     c+=1     x//=10 pro=n**2 rev=ans=0 for i in range(c):     r=pro%10     rev=(rev*10)+r     pro//=10 while rev:     r=rev%10     ans=(ans*10)+r     rev//=10 if ans==n:     print('Automorphic Number') else:     print('Not an Automorphic Number')

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,8 +1,20 @@
-num = int(input())  
-num_of_digits = len(str(num))  
-square = num**2  
-last_digits = square%pow(10,num_of_digits)  
-if last_digits == num:  
-  print("Automorphic Number")  
-else:  
-  print("Not an Automorphic Number")
+n=int(input())
+x=n
+c=0
+while x:
+    c+=1
+    x//=10
+pro=n**2
+rev=ans=0
+for i in range(c):
+    r=pro%10
+    rev=(rev*10)+r
+    pro//=10
+while rev:
+    r=rev%10
+    ans=(ans*10)+r
+    rev//=10
+if ans==n:
+    print('Automorphic Number')
+else:
+    print('Not an Automorphic Number')
