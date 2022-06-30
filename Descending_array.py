d23dbb63d2b05fb8560aac07e933daed48c79188
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-06-30 21:51:30 +0530

    n=int(input()) arr=list(map(int,input().split())) ans=1 for i in range(n-1):     if arr[i]<=arr[i+1]:         ans=0         break if ans==1:     print('yes') else:     print('no')

---
+++ Descending_array.py
@@ -0,0 +1,11 @@
+n=int(input())
+arr=list(map(int,input().split()))
+ans=1
+for i in range(n-1):
+    if arr[i]<=arr[i+1]:
+        ans=0
+        break
+if ans==1:
+    print('yes')
+else:
+    print('no')
