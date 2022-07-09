15acc085fa2a9be21a86de281dfeb023584dd6dd
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-07-09 14:03:43 +0530

    n=int(input()) arr=list(map(int,input().split())) ans=0 for i in range(n):     if arr[i]%2!=0:         if i%2==0:             ans+=1             print('False')             break if ans==0:     print('True')

---
+++ Strictly_ODD.py
@@ -0,0 +1,11 @@
+n=int(input())
+arr=list(map(int,input().split()))
+ans=0
+for i in range(n):
+    if arr[i]%2!=0:
+        if i%2==0:
+            ans+=1
+            print('False')
+            break
+if ans==0:
+    print('True')
