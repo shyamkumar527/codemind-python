8b74727ec2112f9c41225bbfe704167676c9d210
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-07-09 19:36:20 +0530

    s=input() ans=0 if 'a' not in s:     ans=1 if 'e' not in s:     ans=1 if 'i' not in s:     ans=1 if 'o' not in s:     ans=1 if 'u' not in s:     ans=1 if ans==0:     print('True') else:     print('False')

---
+++ All_vowels_in_a_string.py
@@ -0,0 +1,16 @@
+s=input()
+ans=0
+if 'a' not in s:
+    ans=1
+if 'e' not in s:
+    ans=1
+if 'i' not in s:
+    ans=1
+if 'o' not in s:
+    ans=1
+if 'u' not in s:
+    ans=1
+if ans==0:
+    print('True')
+else:
+    print('False')
