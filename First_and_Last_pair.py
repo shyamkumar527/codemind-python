ed4701ccdbdc97f9a5c840f339d0a1ef4be14514
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-07-09 15:24:59 +0530

    n=int(input()) arr=list(map(int,input().split())) even=[] odd=[] e=o=0 for i in range((n//2)+1):     even.append(arr[i]) for i in range(n-1,(n//2),-1):     odd.append(arr[i]) evenlen=len(even) oddlen=len(odd) for i in range(n):     if e>=evenlen and o>=oddlen:         break     elif e>=evenlen:         print(odd[o],end=' ')         o+=1     elif o>=oddlen:         print(even[e],end=' ')         e+=1     else:         print(even[e],end=' ')         e+=1         print(odd[o],end=' ')         o+=1 if n%2!=0:     print('0')

---
+++ First_and_Last_pair.py
@@ -0,0 +1,27 @@
+n=int(input())
+arr=list(map(int,input().split()))
+even=[]
+odd=[]
+e=o=0
+for i in range((n//2)+1):
+    even.append(arr[i])
+for i in range(n-1,(n//2),-1):
+    odd.append(arr[i])
+evenlen=len(even)
+oddlen=len(odd)
+for i in range(n):
+    if e>=evenlen and o>=oddlen:
+        break
+    elif e>=evenlen:
+        print(odd[o],end=' ')
+        o+=1
+    elif o>=oddlen:
+        print(even[e],end=' ')
+        e+=1
+    else:
+        print(even[e],end=' ')
+        e+=1
+        print(odd[o],end=' ')
+        o+=1
+if n%2!=0:
+    print('0')
