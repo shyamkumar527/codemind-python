70653073869a7a67e56f20d4ec6467bfb8305bef
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-07-09 20:10:16 +0530

    s=input() ans=100 res=0 for i in s.split():     c=0     for j in i:         if j in 'aeiouAEIOU':             c+=1     if c<ans:         ans=c for i in s.split():     c=0     for j in i:         if j in 'aeiouAEIOU':             c+=1     if c==ans:         res+=1 print(res)

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,18 @@
+s=input()
+ans=100
+res=0
+for i in s.split():
+    c=0
+    for j in i:
+        if j in 'aeiouAEIOU':
+            c+=1
+    if c<ans:
+        ans=c
+for i in s.split():
+    c=0
+    for j in i:
+        if j in 'aeiouAEIOU':
+            c+=1
+    if c==ans:
+        res+=1
+print(res)
