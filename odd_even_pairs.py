718cf8dba39c8441910a30867c3729378f786720
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-07-09 15:03:21 +0530

    n=int(input()) arr=list(map(int,input().split())) even=[] odd=[] e=o=0 for i in arr:     if i%2==0:         even.append(i)     else:         odd.append(i) evenlen=len(even) oddlen=len(odd) for i in range(n):     if e>=evenlen and o>=oddlen:         break     elif e>=evenlen:         print(odd[o],end=' ')         o+=1     elif o>=oddlen:         print(even[e],end=' ')         e+=1     else:         print(odd[o],end=' ')         o+=1         print(even[e],end=' ')         e+=1 if n%2!=0:     print('0')

---
+++ odd_even_pairs.py
@@ -0,0 +1,28 @@
+n=int(input())
+arr=list(map(int,input().split()))
+even=[]
+odd=[]
+e=o=0
+for i in arr:
+    if i%2==0:
+        even.append(i)
+    else:
+        odd.append(i)
+evenlen=len(even)
+oddlen=len(odd)
+for i in range(n):
+    if e>=evenlen and o>=oddlen:
+        break
+    elif e>=evenlen:
+        print(odd[o],end=' ')
+        o+=1
+    elif o>=oddlen:
+        print(even[e],end=' ')
+        e+=1
+    else:
+        print(odd[o],end=' ')
+        o+=1
+        print(even[e],end=' ')
+        e+=1
+if n%2!=0:
+    print('0')
