a768fa7cbf75a643cbace3a8c8a79f118a9a4386
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-11-02 19:47:23 +0530

    s=input() l=s.split() for i in range(len(l)-1,-1,-1):     x=l[i]     print(x[-1::-1],end=" ")

--- reverse_words_and_positions.py
+++ reverse_words_and_positions.py
@@ -1,8 +1,5 @@
 s=input()
-c=0
-l=[]
-for i in s.split():
-    l.append(i[::-1])
-    c+=1
-for i in range(c-1,-1,-1):
-    print(l[i],end=' ')
+l=s.split()
+for i in range(len(l)-1,-1,-1):
+    x=l[i]
+    print(x[-1::-1],end=" ")
