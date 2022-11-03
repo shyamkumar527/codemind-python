d17433c2d3e857b952249fe1946ba5c36be078f3
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-11-03 19:40:09 +0530

    s=input() d={} for i in s.lower():     if i not in d:         d[i]=1     else:         d[i]+=1 a="abcdefghijklmnopqrstuvwxyz" for i in a:     if i in d and d[i]==1:         print(i,end="")

---
+++ Distinct_characters.py
@@ -0,0 +1,11 @@
+s=input()
+d={}
+for i in s.lower():
+    if i not in d:
+        d[i]=1
+    else:
+        d[i]+=1
+a="abcdefghijklmnopqrstuvwxyz"
+for i in a:
+    if i in d and d[i]==1:
+        print(i,end="")
