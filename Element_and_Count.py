bb3787208fb5fab07b5a6c7b81573ccbce59c2d2
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-11-07 22:53:20 +0530

    n=int(input()) arr=list(map(int,input().split())) l=[] ans=[] for i in arr:     if i not in l:         l.append(i)         ans.append(i)         x=arr.count(i)         ans.append(x) print(*ans)

--- Element_and_Count.py
+++ Element_and_Count.py
@@ -1,8 +1,11 @@
 n=int(input())
 arr=list(map(int,input().split()))
 l=[]
+ans=[]
 for i in arr:
     if i not in l:
         l.append(i)
+        ans.append(i)
         x=arr.count(i)
-        print(i,x,end=' ')
+        ans.append(x)
+print(*ans)
