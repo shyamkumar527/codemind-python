d3b56ee2970302a75fd6d9d563e46b1ec276ef9c
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-11-07 23:14:52 +0530

    n=int(input()) arr=list(map(int,input().split())) l=[] ans=0 for i in arr:     if i not in l:         l.append(i)         if arr.count(i)==i:             ans+=1 print(ans)

--- value_same_as_count.py
+++ value_same_as_count.py
@@ -3,8 +3,8 @@ arr=list(map(int,input().split()))
 l=[]
 ans=0
 for i in arr:
-    x=arr.count(i)
-    if x==i and i not in l:
+    if i not in l:
         l.append(i)
-        ans+=1
+        if arr.count(i)==i:
+            ans+=1
 print(ans)
