948b285097643d9e43bb6771c5118c31706fc9ff
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-11-07 23:16:27 +0530

    n=int(input()) arr=list(map(int,input().split())) l=[] k=int(input()) ans=0 for i in arr:     if i not in l:         l.append(i)         if arr.count(i)==k:             print(i,end=" ")             ans=1 if ans==0:     print("-1")

--- N_times_repeated_elements.py
+++ N_times_repeated_elements.py
@@ -1,15 +1,13 @@
 n=int(input())
 arr=list(map(int,input().split()))
-k=int(input())
 l=[]
-c=0
+k=int(input())
+ans=0
 for i in arr:
-    x=arr.count(i)
-    if x==k and i not in l:
+    if i not in l:
         l.append(i)
-        c=1
-if c==0:
-    print('-1')
-else:
-    for i in l:
-        print(i,end=' ')
+        if arr.count(i)==k:
+            print(i,end=" ")
+            ans=1
+if ans==0:
+    print("-1")
