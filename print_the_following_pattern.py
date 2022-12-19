65395e4dc68892166503d876c31abc7cf18b7c9e
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-12-19 17:46:35 +0530

    n=int(input()) a=0 for i in range(n):     if i==0:         print("*")     elif i==n-1:         for j in range(n):             print("*",end="")     else:         print("*",end="")         j=1         while(1):             if j==a:                 print("*")                 break             else:                 print(" ",end="")             j+=1     a+=1

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,6 +1,19 @@
 n=int(input())
-for i in range(n,0,-1):
-    x=chr(i+64)
-    for j in range(1,i+1):
-        print(x,end=' ')
-    print()
+a=0
+for i in range(n):
+    if i==0:
+        print("*")
+    elif i==n-1:
+        for j in range(n):
+            print("*",end="")
+    else:
+        print("*",end="")
+        j=1
+        while(1):
+            if j==a:
+                print("*")
+                break
+            else:
+                print(" ",end="")
+            j+=1
+    a+=1
