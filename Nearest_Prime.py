d5ca62b9c9ebb9d9b2f184d968822db30c592503
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2023-01-30 17:19:35 +0530

    def prime(n):     if n<2:         return 0     for i in range(2,int(n**0.5)+1):         if n%i==0:             return 0     return 1
    for i in range(int(input())):
        n=int(input())
        a=n
        b=n
        while(1):
            if(prime(a)):
                print(a)
                break
            if(prime(b)):
                print(b)
                break
            a-=1
            b+=1

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,20 +1,21 @@
 def prime(n):
     if n<2:
         return 0
-    for i in range(2,n//2+1):
+    for i in range(2,int(n**0.5)+1):
         if n%i==0:
             return 0
     return 1
+    
 for i in range(int(input())):
     n=int(input())
-    x=n
-    y=n
+    a=n
+    b=n
     while(1):
-        if(prime(x)):
-            print(x)
+        if(prime(a)):
+            print(a)
             break
-        if(prime(y)):
-            print(y)
+        if(prime(b)):
+            print(b)
             break
-        x-=1
-        y+=1
+        a-=1
+        b+=1
