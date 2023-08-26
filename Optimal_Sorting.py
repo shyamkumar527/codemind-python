447ae8b52f6ef66ff41132672e5c3b76a0b824a7
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2023-08-26 14:07:29 +0530

    t=int(input()) for i in range(t):     n=int(input())     l=list(map(int,input().split()))     f=0     for j in range(1,n):         if(l[j-1]>l[j]):             f=1             break     if f==0:         print(0)     else:         print(max(l)-min(l))

--- Optimal_Sorting.py
+++ Optimal_Sorting.py
@@ -4,10 +4,10 @@ for i in range(t):
     l=list(map(int,input().split()))
     f=0
     for j in range(1,n):
-        if l[j]<l[j-1]:
+        if(l[j-1]>l[j]):
             f=1
             break
-    if f==1:
-        print(max(l)-min(l))
-    else:
+    if f==0:
         print(0)
+    else:
+        print(max(l)-min(l))
